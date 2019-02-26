7690614d80a783cad546b652901df0c4d02ba486
Zach Nusbaum <zach@zdnmail.com>
2019-02-26 14:53:47 -0800

    Add react-router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import { Container } from 'semantic-ui-react';
 import Menu from '../Menu';
+
+const Placeholder = () => <div>Placeholder</div>;
 
 class App extends Component {
   render() {
     return (
       <Container>
         <Menu />
+        <Router>
+          <div>
+            <Route path="/" exact component={Placeholder} />
+          </div>
+        </Router>
       </Container>
     );
   }
